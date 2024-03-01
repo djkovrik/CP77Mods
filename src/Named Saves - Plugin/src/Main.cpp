e35656cd6bc02ff17af07ad2eba865e47e40b3aa
DJ_Kovrik <djkovrik@gmail.com>
2024-03-01 12:15:24 +0300

    Named Saves 2.12 update

--- src/Named Saves - Plugin/src/Main.cpp
+++ src/Named Saves - Plugin/src/Main.cpp
@@ -73,8 +73,8 @@ RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo* aInfo)
 {
     aInfo->name = L"NamedSaves";
     aInfo->author = L"DJ_Kovrik";
-    aInfo->version = RED4EXT_SEMVER(2, 4, 0);
-    aInfo->runtime = RED4EXT_RUNTIME_LATEST;
+    aInfo->version = RED4EXT_SEMVER(2, 5, 0);
+    aInfo->runtime = RED4EXT_RUNTIME_INDEPENDENT;
     aInfo->sdk = RED4EXT_SDK_LATEST;
 }
 
