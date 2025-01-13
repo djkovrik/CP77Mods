030bd29958a417915a84d5e69fb5413d70cdf53c
DJ_Kovrik <djkovrik@gmail.com>
2025-01-13 10:17:09 +0300

    Sleeves: setup cli

---
+++ src/Sleeves/pack_release.bat
@@ -0,0 +1,4 @@
+"%~dp0\..\..\..\cli\red-cli.exe" bundle
+echo f | xcopy /f /y Release\r6\scripts\Sleeves\Sleeves.Global.reds Release\r6\scripts\sleeves.reds
+rmdir /s /q Release\r6\scripts\Sleeves
+pause
